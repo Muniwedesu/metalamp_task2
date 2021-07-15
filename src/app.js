9995634ea45104821bcdaa5927c89ef11754cbe6
Muniwedesu <mfmffmm@gmail.com>
2021-07-16 01:30:43 +0300

    added some components and scss imports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-import "./templates/views/style.scss";
+import "./views/global.scss";
+
+//make this run only during compilation
+function requireAll(r) {
+	r.keys().forEach((k) => {}); //(key) => (cache[key] = r(key))
+}
+
+//adds all .js files(which load styles) in components to context
+requireAll(require.context("./components", true, /\.js$/, "sync"));
