514198154928b71e16cf6b2eaf93c7ab92ce9183
Muniwedesu <mfmffmm@gmail.com>
2021-10-10 22:24:36 +0300

    moved some lists to a separate folder

---
+++ src/components/card-details/card-details.js
@@ -0,0 +1,15 @@
+import { Form } from "../card/card";
+
+class DetailsForm extends Form {
+  constructor() {
+    super();
+    //get room number maybe?
+    //get price per day
+    //room-info__price = price;
+    //in summary-list__price:
+    //summary-list__key = `${price} x ${duration}`;
+    //summary-list__value = `${price*duration}`;
+    //get summary list
+    //and total value
+  }
+}
