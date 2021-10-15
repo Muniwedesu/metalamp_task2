7f3184ea4ab190a6d06168bfc2a89c6322361fd1
Muniwedesu <mfmffmm@gmail.com>
2021-10-15 04:53:00 +0300

    implement search page

--- src/views/search/search.js
+++ src/views/search/search.js
@@ -1,3 +1,7 @@
+import { Calendar } from "../../components/calendar/calendar";
+import { CardRoom } from "../../components/card-room/card-room";
+import { CheckboxGroup } from "../../components/checkbox/checkboxGroup";
+import { DropdownMenu } from "../../components/dropdown/dropdown";
 import { Grid } from "../utility";
 
 const SEARCH_ROOMS_GRID_BREAKPOINTS = { min: 472, max: 714 };
@@ -10,9 +14,31 @@ const GRID_SIZES = ["small", "medium", "large"].map((x) => {
 
 export class SearchPage {
   constructor() {
+    //detect all of the cards
+    //and other interactive elemtns
+    // filters menu
+    // link dropdowns
+    //link calendar range
+    //make menu with additional things
+    console.log("search ctor");
     const grid = new Grid(GRID_CONTAINER, {
       breakpoints: SEARCH_ROOMS_GRID_BREAKPOINTS,
       sizes: GRID_SIZES,
     });
+
+    this.calendar = new Calendar($(".dropdown__date").parents(".dropdown"));
+    this.dropdowns = $(".dropdown")
+      .contents(".dropdown__input")
+      .each((x, y) => {
+        new DropdownMenu($(y).parents(".dropdown"));
+      });
+    this.roomCards = $(".room-card")
+      .map((x, elem) => {
+        new CardRoom({ card: elem });
+      })
+      .toArray();
+    this.$checkboxGroup = $(".checkbox-group_expandable");
+    this.checkboxGroup = new CheckboxGroup(this.$checkboxGroup);
   }
 }
+const searchPage = new SearchPage();
