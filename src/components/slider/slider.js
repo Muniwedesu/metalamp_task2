a83838bb0a1f7e43c5342faa02a51d9cbe728925
Muniwedesu <mfmffmm@gmail.com>
2021-10-18 03:08:30 +0300

    implement slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,5 +1,7 @@
 import noUiSlider from "nouislider";
-// import "nouislider/dist/nouislider.css";
+import cssClasses from "nouislider";
+import "nouislider/dist/nouislider.css";
+import "./slider.scss";
 export class Slider {
   /**
    *
@@ -8,31 +10,73 @@ export class Slider {
   constructor(slider) {
     console.log("slider ctor");
     console.log(slider);
-    this.slider = slider;
-    noUiSlider.create(this.slider, {
+    this.$slider = slider;
+    this.$sliderContainer = slider.querySelector("#range-slider");
+    this.minValueTooltip = slider.querySelector(".slider__tooltip_min");
+    this.maxValueTooltip = slider.querySelector(".slider__tooltip_max");
+    noUiSlider.cssClasses.handle += " slider__handle";
+    this.slider = noUiSlider.create(this.$sliderContainer, {
       range: {
-        min: 120,
-        max: 250,
+        min: 3000,
+        max: 20000,
       },
-      step: 30,
-      start: [150, 180],
-      margin: 30,
+      // step: 50,
+      start: [3000, 20000],
+      // margin: 30,
       // limit: 70,
       connect: true,
       direction: "ltr",
       orientation: "horizontal",
       behaviour: "tap-drag",
-      tooltips: true,
+      // tooltips: true,
       // format
-      pips: {
-        mode: "steps",
-        stepped: true,
-        density: 4,
-      },
-      // cssPrefix: "noUi-",
-      // cssClasses: {
-      //   target: "target",
+      // pips: {
+      //   mode: "steps",
+      //   stepped: true,
+      //   density: 4,
       // },
     });
+    this.slider.on(
+      "update",
+      function (values, handle) {
+        console.log("start");
+        let value = String(Math.round(values[handle]));
+        // value = value.split("").reverse().join("");
+        value = value.replace(/(\d{3})/g, " $1");
+        // value = value.split("").reverse().join("");
+        // console.log("end");
+        if (handle) {
+          this.maxValueTooltip.textContent = `${value}Р`;
+        } else {
+          this.minValueTooltip.textContent = `${value}Р`;
+        }
+      }.bind(this)
+    );
   }
 }
+
+// handleLower: "handle-lower",
+// handleUpper: "handle-upper",
+// touchArea: "touch-area",
+// vertical: "vertical",
+// background: "background",
+// draggable: "draggable",
+// drag: "state-drag",
+// tap: "state-tap",
+// active: "active",
+// tooltip: "tooltip",
+// pips: "pips",
+// pipsHorizontal: "pips-horizontal",
+// pipsVertical: "pips-vertical",
+// marker: "marker",
+// markerHorizontal: "marker-horizontal",
+// markerVertical: "marker-vertical",
+// markerNormal: "marker-normal",
+// markerLarge: "marker-large",
+// markerSub: "marker-sub",
+// value: "value",
+// valueHorizontal: "value-horizontal",
+// valueVertical: "value-vertical",
+// valueNormal: "value-normal",
+// valueLarge: "value-large",
+// valueSub: "value-sub"
