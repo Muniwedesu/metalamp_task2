92fbea23eb3deb7955fdf2c5c1009b68f5e55628
Muniwedesu <mfmffmm@gmail.com>
2021-11-02 02:49:20 +0300

    links animations stuff

--- src/components/links-list/links-list.js
+++ src/components/links-list/links-list.js
@@ -1,33 +1,62 @@
 import anime from "animejs";
 const circleSize = 8;
-function createOrFindCircle(target) {
-  let circle = target.querySelector("div");
-  // console.log(circle);
-  if (!circle) {
-    circle = document.createElement("div");
-    circle.style.width = `${circleSize}px`;
-    circle.style.height = `${circleSize}px`;
-    circle.style.position = "absolute";
-    circle.style.borderRadius = "100%";
-    circle.style.zIndex = "-1";
-    circle.style.backgroundColor = "#000";
-    circle.style.opacity = 0;
-    // circle.style.transform = "translateX(-50%)";
-    // circle.style.left = "50%";
-    circle.style.left = 0;
-    circle.style.right = 0;
-    circle.style.marginRight = "auto";
-    circle.style.marginLeft = "auto";
-    circle.style.transform = "translateY(-50%)";
-    circle.style.top = "50%";
-    target.appendChild(circle);
-  }
+function createObjectToAnimate(target) {
+  const circle = document.createElement("div");
+  circle.classList.add("animation__circle");
 
-  target.parentNode.style.overflow = "hidden";
-  // target.parentNode.style.border = "1px solid black";
+  //move all of this to css?
+  circle.style.width = `${circleSize}px`;
+  circle.style.height = `${circleSize}px`;
+  circle.style.position = "absolute";
+  circle.style.borderRadius = "100%";
+  circle.style.backgroundColor = "#000";
+  circle.style.opacity = 0;
+  // circle.style.transform = "translateX(-50%)";
+  // circle.style.left = "50%";
+  circle.style.left = 0;
+  circle.style.right = 0;
+  circle.style.marginRight = "auto";
+  circle.style.marginLeft = "auto";
+  circle.style.transform = "translateY(-50%)";
+  circle.style.top = "50%";
 
-  return circle;
+  const animationWrapper = document.createElement("div");
+
+  animationWrapper.classList.add("animation");
+  animationWrapper.style.zIndex = "-1";
+  animationWrapper.style.overflow = "hidden";
+  animationWrapper.style.position = "absolute";
+  animationWrapper.style.top = 0;
+  animationWrapper.style.left = 0;
+  // animationWrapper.style.backgroundColor = "rgba(255,0,0,0.5)";
+  // this should be 90% for inline links
+  console.log(`target`);
+  console.log(target);
+  //this should check if list-link__item_inline class present;
+  //fix classes for expandable lists, they should not contain this class
+  console.log(target.parentNode);
+  console.log(target.parentNode.classList);
+  console.log(target.parentNode.classList.contains("links-list__item_inline"));
+  animationWrapper.style.width = target.parentNode.classList.contains("links-list__item_inline")
+    ? "120%"
+    : "90%";
+
+  //then center it somehow
+  console.log(animationWrapper.style.width);
+  animationWrapper.style.transform = "translateX(-10%)";
+  animationWrapper.style.height = "100%";
+  animationWrapper.style.borderRadius = "2rem";
+
+  animationWrapper.appendChild(circle);
+
+  return animationWrapper;
 }
+
+function findObjectToAnimate(target) {
+  //
+  return target.querySelector(".animation");
+}
+
 class Link {
   constructor(el) {
     this.$el = el;
@@ -35,19 +64,31 @@ class Link {
     this.$el.addEventListener("mouseenter", this.animateEnter.bind(this));
   }
   //make it prettier or whatever
-  enteringAnimation(circle, event) {
-    const parentDims = {
-      height: event.target.parentNode.getBoundingClientRect().height,
-      width: event.target.parentNode.getBoundingClientRect().width,
-    };
+  createEnteringAnimation(animationObject, event) {
+    //get wrapper dimensions
+    // console.log("received circle");
+    const circle = animationObject.children[0];
+    // console.log(animationObject.children);
 
-    let c = parentDims.height;
-    let r = parentDims.width / 2;
+    //add wrapper to the link node
+    event.target.appendChild(animationObject);
+
+    //this should happpen after DOM is ready
+    const parentDimensions = {
+      height: animationObject.getBoundingClientRect().height,
+      width: animationObject.getBoundingClientRect().width,
+    };
+    //get circle, which is the only element inside wrapper
+    // console.log("parent dimensions");
+    // console.log(parentDimensions);
+    //calculate circle size
+    let c = parentDimensions.height;
+    let r = parentDimensions.width / 2;
 
     let h = r - Math.sqrt(r * r - c * c * 0.25);
-    if (!h) h = parentDims.height * 0.25;
+    if (!h) h = parentDimensions.height * 0.25;
 
-    let ratio = (parentDims.width + 2 * h) / circleSize;
+    let ratio = (parentDimensions.width + 2 * h) / circleSize;
     return anime
       .timeline({})
       .add({
@@ -55,49 +96,42 @@ class Link {
         opacity: [0, 0.15],
         scale: [1, ratio],
         easing: "easeOutSine",
-        duration: 200,
+        duration: 500,
       })
-      .add(
-        {
-          targets: circle,
-          opacity: [0.15, 0.03],
-          easing: "linear",
-          duration: 200,
-          complete: () => {
-            //set target to menu item?
-            //set background to gray color
-            //remove circle
-            //remove overflow: hidden
-            this.$el.parentNode.style.backgroundColor = "rgba(0, 0, 0, 0.03)";
-            this.$el.parentNode.style.overflow = "visible";
-            circle.remove();
-          },
+      .add({
+        opacity: [0.15, 0],
+        duration: 500,
+        complete: () => {
+          animationObject.style.backgroundColor = "rgba(0,0,0,0.03)";
         },
-        "-=100"
-      );
+      });
   }
-  leavingAnimation(circle) {
+  createLeavingAnimation(animationObject) {
     //tidy up
-    let currentOpacity = circle.style.opacity;
+    //animation object should be used there
+    let currentOpacity = animationObject.style.opacity;
     return anime({
-      targets: circle,
+      targets: animationObject,
       opacity: [currentOpacity, 0],
-      duration: 100,
+      duration: 500,
       easing: "linear",
       // background: "rgb(255,0,0)",
       complete: () => {
-        circle.remove();
+        animationObject.remove();
       },
     });
   }
   animateEnter(event) {
     // correctly process entering when animation is not finished;
-    // spawn element on cursor position
+    // center element on cursor position
+
     if (event.target.classList.contains("links-list__link")) {
       setTimeout(() => {
-        const circle = createOrFindCircle(event.target);
+        const animationObject =
+          findObjectToAnimate(event.target) || createObjectToAnimate(event.target);
         // console.log(this.animation);
-        this.animation = this.enteringAnimation(circle, event);
+        //get a wrapper
+        this.animation = this.createEnteringAnimation(animationObject, event);
       }, 0);
     }
   }
@@ -105,17 +139,20 @@ class Link {
     if (event.target.classList.contains("links-list__link")) {
       setTimeout(() => {
         // this.animation.pause();
-        const circle = createOrFindCircle(event.target);
+        const animationObject =
+          findObjectToAnimate(event.target) || createObjectToAnimate(event.target);
+        // const circle = animationObject.children[0];
+        //
         if (this.animation.completed || this.animation.children[0].completed) {
           this.animation.pause();
           // console.log(this.animation);
           // console.log("completed");
-          this.leavingAnimation(circle);
+          this.createLeavingAnimation(animationObject);
         } else {
           this.animation.children[0].finished.then(() => {
             this.animation.pause();
             //start this after previous animation was completed
-            this.leavingAnimation(circle);
+            this.createLeavingAnimation(animationObject);
           });
         }
       }, 0);
@@ -125,7 +162,8 @@ class Link {
 export class LinksList {
   constructor({ list }) {
     this.$el = $(list);
-    this.$items = this.$el.find(".links-list__item");
+    console.log(this.$el);
+    this.$items = this.$el.children(".links-list__item");
     if (window.innerWidth < 1024) {
       this.$el.removeClass("links-list_inline");
       this.$items.removeClass("links-list__item_inline");
@@ -153,8 +191,11 @@ export class LinksList {
       if (event.relatedTarget !== event.target.previousElementSibling)
         event.target.classList.remove("links-list__expandable-list_expanded");
     });
-    this.$items
-      .children(".links-list__link")
+    this.createLinkObjects();
+  }
+  createLinkObjects() {
+    this.$el
+      .find(".links-list__link")
       .toArray()
       .forEach((element) => {
         new Link(element);
